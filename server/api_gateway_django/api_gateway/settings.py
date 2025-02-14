044c30d5d4020969881aafe8156cd0c59db7b9b6
nasif-muhamed <muhdnasifk@gmail.com>
2025-02-14 21:48:54 +0530

    feat: Api Router new app, feat: api_gateway/urls added api_router, feat: api_router/urls.py created new, feat: api_router/views not proper, feat: CORS&REST in settings.py

--- server/api_gateway_django/api_gateway/settings.py
+++ server/api_gateway_django/api_gateway/settings.py
@@ -37,11 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'api_router',  
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware', # for CORS
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -116,3 +121,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWS_CREDENTIALS = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     "https://example.com",
+#     "https://sub.example.com",
+# ]
---
+++ server/api_gateway_django/api_router/views.py
@@ -0,0 +1,69 @@
+import requests
+from django.http import JsonResponse
+from django.http import HttpResponse
+from django.conf import settings
+from rest_framework.decorators import api_view
+from django.shortcuts import HttpResponse
+import os
+
+# URLs of the other services
+USER_SERVICE_URL = 'http://localhost:8001/'  #  os.getenv('ORDER_SERVICE_URL')
+ADMIN_SERVICE_URL = 'http://localhost:8002/'  #  os.getenv('USER_SERVICE_URL')
+# PRODUCT_SERVICE_URL = 'http://localhost:8002/'  #  os.getenv('PRODUCT_SERVICE_URL')
+
+# Proxy view to User Service
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def proxy_to_user_service(request):
+    url = USER_SERVICE_URL + request.path
+    print('url: ', url) 
+    response = requests.request(
+        method=request.method,
+        url=url,
+        headers=request.headers,
+        json=request.data,
+    )
+    
+    return HttpResponse(
+        response.content,
+        status=response.status_code,
+        content_type=response.headers['Content-Type']
+    )
+
+# Proxy view to Product Service
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def proxy_to_product_service(request):
+    url = PRODUCT_SERVICE_URL + request.path
+
+    response = requests.request(
+        method=request.method,
+        url=url,
+        headers=request.headers,
+        json=request.data,
+    )
+    
+    return HttpResponse(
+        response.content,
+        status=response.status_code,
+        content_type=response.headers['Content-Type']
+    )
+
+# Proxy view to Order Service
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def proxy_to_order_service(request):
+    url = ORDER_SERVICE_URL + request.path
+
+    response = requests.request(
+        method=request.method,
+        url=url,
+        headers=request.headers,
+        json=request.data,
+    )
+    
+    return HttpResponse(
+        response.content,
+        status=response.status_code,
+        content_type=response.headers['Content-Type']
+    )
+
+def hello_world(request):
+    return HttpResponse('Hello World')
