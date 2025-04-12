08e990f533d64e07a1e6e32f1614ea6c95c6a52c
nasif-muhamed <muhdnasifk@gmail.com>
2025-04-12 08:44:38 +0530

    refactor: StudentCourseDetails.jsx - implemented dummysubscription option without stripe, just showing rating and total_ranting count

--- client/src/pages/user/student/courses/StudentCourseDetails.jsx
+++ client/src/pages/user/student/courses/StudentCourseDetails.jsx
@@ -99,11 +99,15 @@ const StudentCourseDetails = () => {
             const body = {
                 course: id,
                 purchase_type: purchaseType,
-                // subscription_amount: course?.subscription_amount,
-                // video_session: course?.video_session,
-                // chat_upto: course?.chat_upto,
-                // safe_period: course?.safe_period,
             };
+
+            if (purchaseType === "subscription") {
+                body.subscription_amount = course?.subscription_amount;
+                body.video_session = course?.video_session;
+                body.chat_upto = course?.chat_upto;
+                body.safe_period = course?.safe_period;
+            }
+            console.log("Purchase body:", body);
             const response = await api.post(`courses/${id}/purchase/`, body);
             console.log("Purchase response:", response);
             toast.success("Course purchased successfully!");
@@ -192,7 +196,7 @@ const StudentCourseDetails = () => {
                                         </div>
 
                                         {course?.subscription && course?.is_enrolled !== 'subscription' &&  (
-                                            <button className="btn-primary w-full mb-2">
+                                            <button onClick={()=>{handlePurchase('subscription')}} className="btn-primary hover:bg-blue-500 w-full mb-2">
                                                 Subscribe
                                             </button>
                                         )}
@@ -451,7 +455,7 @@ const StudentCourseDetails = () => {
                                         <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
                                     </svg>
                                     <span className="font-bold mr-1">
-                                        {courseData.rating}
+                                        {course?.average_rating}
                                     </span>
                                     <span className="text-gray-400">
                                         course rating
@@ -460,7 +464,7 @@ const StudentCourseDetails = () => {
                                         •
                                     </span>
                                     <span className="text-gray-400">
-                                        7K ratings
+                                        {course?.total_reviews} ratings
                                     </span>
                                 </div>
 
