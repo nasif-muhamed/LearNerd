003d4b7dd53cbee88d7b9506105409ef88b7ab88
nasif-muhamed <muhdnasifk@gmail.com>
2025-10-28 20:52:58 +0530

    fix(client/auth): moved exposed firebase keys into .env

--- client/src/services/firebase/firebase.js
+++ client/src/services/firebase/firebase.js
@@ -1,13 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
+const FIREBASE_API_KEY = import.meta.env.VITE_FIREBASE_API_KEY;
+const FIREBASE_AUTH_DOMAIN = import.meta.env.VITE_FIREBASE_AUTH_DOMAIN;
+const FIREBASE_PROJECT_ID = import.meta.env.VITE_FIREBASE_PROJECT_ID;
+const FIREBASE_STORAGE_BUCKET = import.meta.env.VITE_FIREBASE_STORAGE_BUCKET;
+const FIREBASE_MESSAGING_SENDER_ID = import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID;
+const FIREBASE_APP_ID = import.meta.env.VITE_FIREBASE_APP_ID;
+
 const firebaseConfig = {
-    apiKey: "AIzaSyA37m6g-_f8iIyF0jZBc-1hX7PpllHkyq8",
-    authDomain: "learnerds-1bd6f.firebaseapp.com",
-    projectId: "learnerds-1bd6f",
-    storageBucket: "learnerds-1bd6f.firebasestorage.app",
-    messagingSenderId: "1094541551100",
-    appId: "1:1094541551100:web:ff6747a912ef5ddf823e79"
+    apiKey: FIREBASE_API_KEY,
+    authDomain: FIREBASE_AUTH_DOMAIN,
+    projectId: FIREBASE_PROJECT_ID,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+    appId: FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
